708cd18a66441930534c772a07d183f6ce4183f0
MarwanHassan <marwanhassan954@fmail.com>
2024-01-22 11:07:18 -0800

    this is my third comment , task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdio.h>
+#include "main.h"
 
 /*
  * this an app to print the name of the program using argc , argv[] 
  */
 
 int main (int argc, char *argv[]) {
+
 
 	printf("%s\n", argv[0]);
 }
