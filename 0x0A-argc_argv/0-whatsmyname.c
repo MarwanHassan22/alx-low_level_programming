19f267caa94a56abfee442216129f93e689ac9bf
MarwanMano <marwanhassan954@gmail.com>
2024-01-22 02:29:52 +0200

    this is my first comment , first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/*
+ * this an app to print the name of the program using argc , argv[] 
+ */
+
+int main (int argc, char *argv[]) {
+
+	printf("%s\n", argv[0]);
+}
+
