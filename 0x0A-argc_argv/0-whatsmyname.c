05806b1974c96c08a1495a99ef87b57d9a608d21
MarwanMano <marwanhassan954@gmail.com>
2024-01-24 01:12:12 +0200

    this is comment of creating the file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,26 @@
-#include "main.h"
-#include <libgen.h>
+#include <stdio.h>
 
-/*
- * this an app to print the name of the program using argc , argv[] 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 on success, 1 on error
  */
-
-int main (int argc, char *argv[]) {
-
-	char *programName = basename(argv[0]);
-
-	printf("%s\n", programName);
+int main(int argc, char *argv[])
+{
+    /* Check if there is at least one argument */
+    if (argc > 0)
+    {
+        /* Print the name of the program (including path) */
+        printf("%s\n", argv[0]);
+        return 0;
+    }
+    else
+    {
+        /* Print an error message if there are no arguments */
+        fprintf(stderr, "Usage: %s <program_name>\n", argv[0]);
+        return 1;
+    }
 }
 
