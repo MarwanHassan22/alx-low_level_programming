13f2c703bfac590b0b258b27eca25a58bb073135
MarwanMano <marwanhassan954@gmail.com>
2024-02-04 14:43:26 +0200

    this is the third task , finished

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 98 on incorrect number of arguments,
+ * 99 on invalid operator, 100 if dividing/modulo by zero
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*operation)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    operation = get_op_func(argv[2]);
+
+    if (!operation)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    result = operation(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
+
