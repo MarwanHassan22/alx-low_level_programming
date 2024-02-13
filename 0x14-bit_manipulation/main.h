f8b58d065e7dc515832791011e1dbc9a355dedb6
MarwanMano <marwanhassan954@gmail.com>
2024-02-13 15:00:20 +0200

    this is the fourth task , finished

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
