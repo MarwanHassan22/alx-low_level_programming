8964a3bbd8123ba967a815edfa05ae72df82a73e
MarwanMano <marwanhassan954@gmail.com>
2024-02-13 15:05:08 +0200

    this is the fifth task , finished

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
