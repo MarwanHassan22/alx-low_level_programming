a22291bb374fd9997ff52cd94246190a8f27ccb5
MarwanMano <marwanhassan954@gmail.com>
2024-02-13 15:10:22 +0200

    this is the first additional task , finished

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+
+int get_endianness(void);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
